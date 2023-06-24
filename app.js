1f05e977eebd2fc802ab0e7aee9f8f72c6a4a9b7
YanBastos <yanmoraesbastos@hotmail.com>
2023-06-23 23:37:14 -0300

    +3

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ const express = require('express'); //importando express
 
 const app = express(); // chamando express para o app, tendo tudo disponivel dentro dele
 
+//Middlewares
+
+//Usando o middleware para verificar se qunado fizer a chamda web vai existir um json ali e 
+//se deve processar deixando disponivel dentro do "body(req)"
+
+app.use(express.json());
+
+
+                        //next passa a requisição para oproximo middleware
+const log = (req, res, next) => {
+    console.log(req.body);
+    console.log(`Data: ${Date.now()}`);
+    next(); //passa para o proximo middleware 
+}
+
+app.use(log);
 
 //ROTAS
 
@@ -19,10 +35,10 @@ app.get('/', (req, res) => { // criando rota do tipo get
 })
 
 app.get('/json', (req, res) => {
+    console.log(req.body); // mostra no console  o json disponivel no nosso body, com middleware
     res.json({title: 'Resposta Json',name:' Yan Bastos', Tarefa: true });
 })
 
-//
 
 app.listen(3000, () => { //escutando a porta 3000 
     console.log('Servidor iniciado')
